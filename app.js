afa7ee343e0e5d09f182e9086d09f6bff0be2ff2
akonihakeem <akozymayor@gmail.com>
2024-10-14 15:41:12 +0100

    created person object

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("I am learning js objects, arrays and functions");
+// object
+const firstname = 'Jamal';
+const lastname = 'Adeleye';
+const age = 20;
+const isMarried = false;
+const address = 'Lagos';
+const skill1 = 'HTML';
+const skill2 = 'CSS';
+const skill3 = 'JavaScript';
+
+let person = {};
+
+// TODO: write a function that takes an object and logs it content
+console.log("this is an empty person object", person);
